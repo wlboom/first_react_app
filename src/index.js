96581b2642e9380090dc81602a6dd7f1cdfd85fd
Frost <frostwalker91@gmail.com>
2021-02-08 22:07:33 +0400

    add react bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './Components/App/App';
+
 
 ReactDOM.render(
   <React.StrictMode>
